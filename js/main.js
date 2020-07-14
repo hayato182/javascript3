a53aaeadf402a00fdbebe2bf7ef7a84e31b4a2a8
hayato182 <joruji25@gmail.com>
2020-07-14 10:41:27 +0900

    指摘箇所を修正

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
 'use strict';
 {
-  let id = -1;
-  function createTr () {
+  let id = 0;
+  const tasks = [];
+
+  let addTasks = () => {
+    const task = document.getElementById('text');
+    tasks.push(task.value);
+    task.value = '';
+  }
+
+  let displayTasks = () => {
     const tr = document.createElement('tr');
     // 一列目
     const td1 = document.createElement('td');
-    td1.textContent = String(id + 1).padStart(3, '0');
+    td1.textContent = id;
     tr.appendChild(td1);
-    id++;
     // 二列目
     const td2 = document.createElement('td');
-    const text = document.getElementById('text');
-    td2.textContent = text.value;
+    td2.textContent = tasks[id];
     tr.appendChild(td2);
-    text.value = '';
-    //三列目
+    // 三列目
     const td3 = document.createElement('td');
     const button1 = document.createElement('button');
-    button1.textContent = "作業中";
-    td3.appendChild(button1)
+    button1.textContent = '作業中';
+    td3.appendChild(button1);
     tr.appendChild(td3);
     // 四列目
     const td4 = document.createElement('td');
     const button2 = document.createElement('button');
-    button2.textContent = "削除";
+    button2.textContent = '削除';
     td4.appendChild(button2)
     tr.appendChild(td4);
     document.querySelector('tbody').appendChild(tr);
+    id++;
   }
+
+
   document.querySelector('button').addEventListener('click', () => {
-    createTr();
+    addTasks();
+    displayTasks();
   });
 }
