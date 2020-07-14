fc5c962c517cf843dd437db1f45051e2cbbdbb22
hayato182 <joruji25@gmail.com>
2020-07-14 21:56:48 +0900

    変数名を具体的に変更

--- js/main.js
+++ js/main.js
@@ -12,25 +12,25 @@
   let displayTasks = () => {
     const tr = document.createElement('tr');
     // 一列目
-    const td1 = document.createElement('td');
-    td1.textContent = id;
-    tr.appendChild(td1);
+    const tdId = document.createElement('td');
+    tdId.textContent = id;
+    tr.appendChild(tdId);
     // 二列目
-    const td2 = document.createElement('td');
-    td2.textContent = tasks[id];
-    tr.appendChild(td2);
+    const tdTask = document.createElement('td');
+    tdTask.textContent = tasks[id];
+    tr.appendChild(tdTask);
     // 三列目
-    const td3 = document.createElement('td');
+    const tdStatus = document.createElement('td');
     const button1 = document.createElement('button');
     button1.textContent = '作業中';
-    td3.appendChild(button1);
-    tr.appendChild(td3);
+    tdStatus.appendChild(button1);
+    tr.appendChild(tdStatus);
     // 四列目
-    const td4 = document.createElement('td');
+    const tdDelete = document.createElement('td');
     const button2 = document.createElement('button');
     button2.textContent = '削除';
-    td4.appendChild(button2)
-    tr.appendChild(td4);
+    tdDelete.appendChild(button2)
+    tr.appendChild(tdDelete);
     document.querySelector('tbody').appendChild(tr);
     id++;
   }
