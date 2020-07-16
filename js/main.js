8841cd2392e89ec7bc30c7c5cb6a11164c47283a
hayato182 <joruji25@gmail.com>
2020-07-16 09:39:38 +0900

    配列の中にオブジェクトを追加しデータを蓄積

--- js/main.js
+++ js/main.js
@@ -2,27 +2,28 @@
 {
   let id = 0;
   const tasks = [];
-
   let addTasks = () => {
+    tasks.push({});
+    tasks[id]['id'] = id;
     const task = document.getElementById('text');
-    tasks.push(task.value);
+    tasks[id]['coment'] = task.value;
     task.value = '';
   }
-
   let displayTasks = () => {
     const tr = document.createElement('tr');
     // 一列目
     const tdId = document.createElement('td');
-    tdId.textContent = id;
+    tdId.textContent = tasks[id]['id'];
     tr.appendChild(tdId);
     // 二列目
     const tdTask = document.createElement('td');
-    tdTask.textContent = tasks[id];
+    tdTask.textContent = tasks[id]['coment'];
     tr.appendChild(tdTask);
     // 三列目
     const tdStatus = document.createElement('td');
     const button1 = document.createElement('button');
-    button1.textContent = '作業中';
+    tasks[id]['status'] = '作業用';
+    button1.textContent = tasks[id]['status'];
     tdStatus.appendChild(button1);
     tr.appendChild(tdStatus);
     // 四列目
@@ -34,10 +35,9 @@
     document.querySelector('tbody').appendChild(tr);
     id++;
   }
-
-
   document.querySelector('button').addEventListener('click', () => {
     addTasks();
     displayTasks();
+    console.log(tasks);
   });
 }
