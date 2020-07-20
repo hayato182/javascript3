07c5b2b0a7aea3c9a8a6fd648bf4b3cc755febb7
hayato182 <joruji25@gmail.com>
2020-07-20 11:56:20 +0900

    ループを使ってオブジェクトを取得

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 {
   let id = 0;
   const tasks = [];
+  const tbody = document.querySelector('tbody')
   let addTasks = () => {
     const taskTextBox = document.getElementById('text');
     let task = {
@@ -13,34 +14,38 @@
     taskTextBox.value = '';
   }
   let displayTasks = () => {
-    const tr = document.createElement('tr');  
-    // 一列目
-    const tdId = document.createElement('td');
-    tdId.textContent = tasks[id]['id'];
-    tr.appendChild(tdId);
-    // 二列目
-    const tdTask = document.createElement('td');
-    tdTask.textContent = tasks[id]['comment'];
-    tr.appendChild(tdTask);
-    // 三列目
-    const tdStatus = document.createElement('td');
-    const button1 = document.createElement('button');
-    // tasks[id]['status'] = '作業中';
-    button1.textContent = tasks[id]['status'];
-    tdStatus.appendChild(button1);
-    tr.appendChild(tdStatus);
-    // 四列目
-    const tdDelete = document.createElement('td');
-    const button2 = document.createElement('button');
-    button2.textContent = '削除';
-    tdDelete.appendChild(button2)
-    tr.appendChild(tdDelete);
-    document.querySelector('tbody').appendChild(tr);
-    id++;
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    for (let i = 0; i < tasks.length; i++) {
+      const tr = document.createElement('tr');  
+      // 一列目
+      const tdId = document.createElement('td');
+      tdId.textContent = tasks[i]['id'];
+      tr.appendChild(tdId);
+      // 二列目
+      const tdTask = document.createElement('td');
+      tdTask.textContent = tasks[i]['comment'];
+      tr.appendChild(tdTask);
+      // 三列目
+      const tdStatus = document.createElement('td');
+      const button1 = document.createElement('button');
+      button1.textContent = tasks[i]['status'];
+      tdStatus.appendChild(button1);
+      tr.appendChild(tdStatus);
+      // 四列目
+      const tdDelete = document.createElement('td');
+      const button2 = document.createElement('button');
+      button2.textContent = '削除';
+      tdDelete.appendChild(button2)
+      tr.appendChild(tdDelete);
+      tbody.appendChild(tr);
+    }
   }
   document.querySelector('button').addEventListener('click', () => {
     addTasks();
     displayTasks();
+    id++;
     console.log(tasks);
   });
 }
