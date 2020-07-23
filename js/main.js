15ea8db11500decc67ae3d98e598125dab6fd9c8
hayato182 <joruji25@gmail.com>
2020-07-23 17:22:04 +0900

    3-2を提出

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 'use strict';
 {
-  let id = 0;
   const tasks = [];
   const tbody = document.querySelector('tbody')
   let addTasks = () => {
     const taskTextBox = document.getElementById('text');
     let task = {
-      'id': id,
+      'id':'',
       'comment': taskTextBox.value,
-      'status': '作業中'
+      'status': '作業中',
+      'delete': '削除',
     };
     tasks.push(task);
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].id = i;
+    };
     taskTextBox.value = '';
   }
   let displayTasks = () => {
@@ -36,15 +39,27 @@
       // 四列目
       const tdDelete = document.createElement('td');
       const button2 = document.createElement('button');
-      button2.textContent = '削除';
-      tdDelete.appendChild(button2)
+      button2.textContent = tasks[i]['delete'];
+      button2.classList.add('delete');
+      tdDelete.appendChild(button2);
       tr.appendChild(tdDelete);
       tbody.appendChild(tr);
     }
+    const deletLists = Array.from(document.querySelectorAll('button.delete'));
+    deletLists.forEach(deletList => {
+      deletList.addEventListener('click', e => {
+        const targetTr = e.target.parentNode.parentNode;
+        const targetId = Number(targetTr.firstChild.textContent);
+          tasks.splice(targetId,1);
+          for (let i = 0; i < tasks.length; i++) {
+            tasks[i].id = i;
+          };
+        displayTasks();
+      });
+    });
   }
   document.querySelector('button').addEventListener('click', () => {
     addTasks();
     displayTasks();
-    id++;
   });
 }
